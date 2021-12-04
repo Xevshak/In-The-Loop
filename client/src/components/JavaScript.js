3e2b9075f7e169eee46cf10ec5dae0ccb8046147
CortezStephanie <cortezstephanie2389@yahoo.com>
2021-12-03 20:41:41 -0600

    JavaScript page content

--- client/src/components/JavaScript.js
+++ client/src/components/JavaScript.js
@@ -1,5 +1,40 @@
 import React from 'react'
 
 export default function JavaScript () {
-    return(null)
+    const [value, setValue] = useState('');
+
+    const handleChange = (e) => {
+        setValue (e.target.value)
+    }
+    return(
+        <div>
+            <Navbar/>
+            <div id="creepBox"><Forloop creep={value}/></div>
+                <pre className='card'>
+                    <code>
+                        <span>
+{`for (let i = 0; i <`}
+                        </span> 
+                        <span> 
+                            <form>
+                                <label>            
+                                    <select value={value} onChange={handleChange}>
+                                    <option value='0'>0</option>
+                                    <option value='5'>5</option>
+                                    <option value='10'>10</option>
+                                    <option value='3'>3</option>
+                                    <option value="1">1</option>
+                                    </select>
+                                </label>          
+                            </form>
+                        </span>
+                        <span>
+{`; i++) {
+    creeper += creeper[i];
+}`}</span>
+                </code>
+            </pre>
+
+        </div>
+    )
 }

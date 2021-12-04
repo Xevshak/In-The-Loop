cd4ede39f5c87f222a1db22de4abd0f2e9e2cdb9
BradMW <bodell94@yahoo.com>
2021-12-04 12:31:56 -0600

    added an else to server.js

--- server/server.js
+++ server/server.js
@@ -23,6 +23,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+else {
+  res.sendFile(path.join(__dirname, '../client/public/'));
+}
+
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));

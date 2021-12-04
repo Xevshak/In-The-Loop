30cb174a074b334a128517d055428dee7ce8d83f
CortezStephanie <cortezstephanie2389@yahoo.com>
2021-12-03 23:38:19 -0600

    deleted extra imag folder

--- client/src/components/Forloop.js
+++ client/src/components/Forloop.js
@@ -4,7 +4,7 @@ export default function Forloop (props) {
     const creepers = [];
 
     for (let i = 0; i < props.creep; i++) {
-        creepers.push(<img src='../../public/assets/img/dirt.jpg' key={i} alt='creeper' className='creeper'></img>)
+        creepers.push(<img src='../../public/assets/images/dirt.jpg' key={i} alt='creeper' className='creeper'></img>)
     }
     return(
         <div>{creepers}</div>

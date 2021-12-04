121872faf3f3484d1c78827ffdc5f3f8d525e2e3
BradMW <bodell94@yahoo.com>
2021-12-04 11:48:31 -0600

    working on routes in API js

---
+++ client/src/utils/API.js
@@ -0,0 +1,51 @@
+// route to get logged in user's info (needs the token)
+export const getMe = (token) => {
+    return fetch('/api/users/me', {
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${token}`,
+      },
+    });
+  };
+  
+  export const createUser = (userData) => {
+    return fetch('/api/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    });
+  };
+  
+  export const loginUser = (userData) => {
+    return fetch('/api/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    });
+  };
+  
+  // save question data and progress for a logged in user
+  export const progress = (questionData, token) => {
+    return fetch('/api/users', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(questionData),
+    });
+  };
+  
+  // get questions from the db
+  export const question = (questionId, token) => {
+    return fetch(`/api/users/books/${questionId}`, {
+      method: 'GET',
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+  };

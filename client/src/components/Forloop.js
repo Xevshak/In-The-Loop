51ceb3a35da14e7bb237433dc0090bcefba7fcf8
Xevshak <keshavsternkrishnan@gmail.com>
2021-12-04 17:26:34 -0500

    fixed creeper img path

--- client/src/components/Forloop.js
+++ client/src/components/Forloop.js
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import creeper from '../assets/images/creeper.png'
+import './Forloop.css'
 export default function Forloop (props) {
     const creepers = [];
 
     for (let i = 0; i < props.creep; i++) {
-        creepers.push(<img src='../../public/assets/images/dirt.jpg' key={i} alt='creeper' className='creeper'></img>)
+        creepers.push(<img src={creeper} key={i} alt='creeper' className='creeper'></img>)
     }
     return(
         <div>{creepers}</div>

5629ee37242010a88813ebfaa2dd39a373ff922b
CortezStephanie <cortezstephanie2389@yahoo.com>
2021-12-03 20:44:47 -0600

    added import navbar to javascript component

--- client/src/components/JavaScript.js
+++ client/src/components/JavaScript.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Navbar from './Navbar'
+import Forloop from './Forloop'
 
 export default function JavaScript () {
     const [value, setValue] = useState('');

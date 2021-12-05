3a93b4af91e69bd851d48929e937263d518eaf9f
Christopher Kennard <84753350+chris79kennard@users.noreply.github.com>
2021-12-04 20:07:22 -0600

    linked css files to footer.js for styling

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import "../Style/Footer.css"
 export default function Footer () {
     return(
         <div>
@@ -19,3 +19,4 @@ export default function Footer () {
     </div>
     )
 }
+

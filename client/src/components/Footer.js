424b219dc613aea2db56bb323f51a38432358cf6
Christopher Kennard <84753350+chris79kennard@users.noreply.github.com>
2021-12-04 20:37:34 -0600

    added git hub and linked in buttons for footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,13 +7,30 @@ export default function Footer () {
         <ul className="contributors">
             <p>Back End Developers</p>
             <li>Bradley</li>
+            <i className="fab fa-linkedin"></i>
+            <i className="fab fa-github"></i>
             <li>Chamo</li>
+            <i className="fab fa-linkedin"></i>
+            <i className="fab fa-github"></i>
             <li>Keshav</li>
+            <i className="fab fa-linkedin"></i>
+            <i className="fab fa-github"></i>
         </ul>
         <ul className='contributors'>    
             <p>Front End Developers</p>
+            
             <li>Christopher</li>
+            <a href="www.linkedin.com/in/christopher-kennard-13a479213" target="_blank" rel="noreferrer"  >
+            <i class="fab fa-linkedin"></i></a>
+            <a href="https://github.com/chris79kennard" target="_blank" rel="noreferrer" >
+            <i class="fab fa-github"></i></a>
+
             <li>Stephanie</li>
+                <a href="https://www.linkedin.com/feed/" target="_blank" rel="noreferrer"  >
+            <i class="fab fa-linkedin"></i></a>
+            <a href="https://github.com/CortezStephanie" target="_blank" rel="noreferrer" >
+            <i class="fab fa-github"></i></a>
+
         </ul>
         </footer>
     </div>

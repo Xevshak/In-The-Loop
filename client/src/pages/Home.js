6d668e1b090d9f0bc85f452d92e2d503abb29518
CortezStephanie <cortezstephanie2389@yahoo.com>
2021-12-05 04:31:35 -0600

    adding style sheets back to js files somehow they arent in frontend

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../Style/Home.css'
+
 export default function Home () {
     return(
         <div>
             
-            <button type='submit'> <Link to={`/login`}>Login</Link></button>
+            <button type='submit'><Link to={`/login`}>Login</Link></button>
             <button type='submit'><Link to={`/signup`}>Sign Up</Link></button>
         </div>
     )

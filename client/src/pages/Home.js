7bccf15d17ef11b2fd986d392f3555ff5b2fbbc8
CortezStephanie <cortezstephanie2389@yahoo.com>
2021-12-06 20:09:31 -0600

    styling login buttons

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,12 +4,17 @@ import '../Style/Home.css'
 
 //import ReactDOM from 'react-dom';
 import Button from '@mui/material/Button';
+import Box  from '@mui/material/Box'
+
 
 export default function Home () {
+
     return(
         <div>
-            <Button variant="contained"><Link to={`/login`}>Login</Link></Button>
-            <Button variant="contained"><Link to={`/signup`}>Sign Up</Link></Button>
+            <Box>
+                <Button variant="contained" className='loginBtn' style={{ background: '#C7BCB9' }} sx={{margin: 1}}><Link to={`/login`}>Login</Link></Button>
+                <Button variant="contained" className='loginBtn' style={{ background: '#C7BCB9' }} sx={{margin: 1}}><Link to={`/signup`}>Sign Up</Link></Button>
+            </Box>
         </div>
     )
 }

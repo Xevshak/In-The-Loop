c3e5617a946284abc6be600a0c23c9023e54c9cd
CortezStephanie <cortezstephanie2389@yahoo.com>
2021-12-07 18:26:46 -0600

    added cards to homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,22 +2,46 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Btnimg from '../images/Btn.png'
 import '../Style/Home.css'
+import Footer from '../components/Footer'
 
 //import ReactDOM from 'react-dom';
+import { Typography } from '@mui/material'
 import Button from '@mui/material/Button';
 import Box  from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import { CardContent } from '@mui/material'
 
 
 export default function Home () {
     
     return(
         <div>
+            <Typography id='homeText' style={{fontFamily: 'Minecraft', color: 'white'}}>Learn How To Code The Fun Way</Typography>
+
             <Box>
-                <Button variant="contained" className='loginBtn' style={{boxShadow: 'inset 2px 2px 1px purple ', border: '2px solid black', borderRadius: 0, cursor: 'pointer', color: '#fafafa', paddingTop:'3px' }} sx={{margin: 1}}><Link to={`/login`} style={{textDecoration: 'none', color: 'white', fontFamily: 'SilkscreenNormal', borderBottom:'2px solid darkgray', padding: '0 2px' }}>Login</Link></Button>
-                <Button variant="contained" className='loginBtn' style={{ background: '#C7BCB9', border: '2px solid black', borderRadius: 0 }} sx={{margin: 1}}><Link to={`/signup`}>Sign Up</Link></Button>
+                <Button variant="contained" className='loginBtn' style={{  backgroundImage: `url(${Btnimg})`, boxShadow: 'inset 2px 2px 1px #e0e0e0', border: '2px solid black', borderRadius: 0, cursor: 'pointer', paddingTop:'3px' }} sx={{margin: 1}}><Link to={`/login`} style={{textDecoration: 'none', color: 'white', fontFamily: 'SilkscreenNormal'}}>Login</Link></Button>
+
+                <Button variant="contained" className='loginBtn' style={{ backgroundImage: `url(${Btnimg})`, boxShadow: 'inset 2px 2px 1px ', border: '2px solid black', borderRadius: 0, cursor: 'pointer', paddingTop:'3px' }} sx={{margin: 1}}><Link to={`/login`} style={{textDecoration: 'none', color: 'white', fontFamily: 'SilkscreenNormal'}}>Sign Up</Link></Button>
             </Box>
+
+            <Card sx={{margin:1}} style={{backgroundColor: 'darkgreen'}}>
+                <CardContent style={{textAlign:'center'}}>
+                    <Typography variant="h5" component="div">CSS TUTORIAL</Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">coming soon!</Typography>
+                </CardContent>
+            </Card>
+
+            <Card sx={{margin:1}} style={{backgroundColor: 'darkgreen'}}>
+                <CardContent style={{textAlign:'center'}}>
+                    <Typography variant="h5" component="div">
+                    HTML TUTORIAL
+                    </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    coming soon!
+                    </Typography>
+                </CardContent>
+            </Card>
+            <Footer/>
         </div>
     )
 }
-
-//backgroundImage: `url(${Btnimg})`
